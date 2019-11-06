ed11964e790e6f604f22cafcd731c45c5d3b17de
Madalina Erascu <madalina.erascu@e-uvt.ro>
2019-11-06 09:40:30 +0200

    added finite domain files

--- utils/toFD.py
+++ utils/toFD.py
@@ -62,14 +62,14 @@ def toFiniteDomain(inputPath, outputPath, useCase):
             with open(inputFile, "rt") as inp:
                 with  open(outputFile, "w+") as out:
                     for line in inp:
-                        cpu = re.match(r"\(declare\-fun\s+p\d_\d\s+\(\)\s+Int\)", line)
-                        mem = re.match(r"\(declare\-fun\s+m\d_\d\s+\(\)\s+Int\)", line)
-                        sto = re.match(r"\(declare\-fun\s+s\d_\d\s+\(\)\s+Int\)", line)
+                        cpu = re.match(r"\(declare\-fun\s+p(\d)*_(\d)*\s+\(\)\s+Int\)", line)
+                        mem = re.match(r"\(declare\-fun\s+m(\d)*_(\d)*\s+\(\)\s+Int\)", line)
+                        sto = re.match(r"\(declare\-fun\s+s(\d)*_(\d)*\s+\(\)\s+Int\)", line)
                         # print("cpu ", cpu)
                         # print("mem ", mem)
                         # print("sto ", sto)
                         if cpu != None:
-                            cmCPU = re.search(r"\d_\d", cpu.group(0)).group(0).split("_")
+                            cmCPU = re.search(r"(\d)*_(\d)*", cpu.group(0)).group(0).split("_")
                             df = re.sub("declare\-fun", "define-fun", cpu.group(0))
                             c = int(cmCPU[0])
                             #print("???", cmpHardwareReqs[c-1]["cpu"])
@@ -77,14 +77,14 @@ def toFiniteDomain(inputPath, outputPath, useCase):
                                 cmpHardwareReqs[c-1]["cpu"]) + " 0))", df)
                             out.write(p+"\n")
                         elif mem != None:
-                            cmMem = re.search(r"\d_\d", mem.group(0)).group(0).split("_")
+                            cmMem = re.search(r"(\d)*_(\d)*", mem.group(0)).group(0).split("_")
                             df = re.sub("declare\-fun", "define-fun", mem.group(0))
                             c = int(cmMem[0])
                             m = re.sub("Int\)", "Int (ite C" + cmMem[0] + "_" + "VM" + cmMem[1] + " " + str(
                                 cmpHardwareReqs[c-1]["mem"]) + " 0))", df)
                             out.write(m+"\n")
                         elif sto != None:
-                            cmSto = re.search(r"\d_\d", sto.group(0)).group(0).split("_")
+                            cmSto = re.search(r"(\d)*_(\d)*", sto.group(0)).group(0).split("_")
                             df = re.sub("declare\-fun", "define-fun", sto.group(0))
                             c = int(cmSto[0])
                             s = re.sub("Int\)", "Int (ite C" + cmSto[0] + "_" + "VM" + cmSto[1] + " " + str(
@@ -97,9 +97,9 @@ def toFiniteDomain(inputPath, outputPath, useCase):
 
 
 def main():
-    toFiniteDomain("../experimentalResults/linear/output_SMT_Solver_Z3_RealPBC_assertsoft/SMT2",
-                   "../experimentalResults/linear/output_SMT_Solver_Z3_FD/SMT2",
-                   "../testInstances/Wordpress3.json")
+    toFiniteDomain("../experimentalResults/linear/output_SMT_Solver_Z3_FD/SMT2",
+                   "../experimentalResults/linear/output_SMT_Solver_Z3_FD-final/SMT2",
+                   "../testInstances/Oryx2.json")
 
 
 if __name__ == "__main__":
